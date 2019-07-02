8566d34f4acba32655cdd43afca35b4daeb6e553
Naoki Harima <hurry4170@gmail.com>
2019-07-03 08:43:44 +0900

    Parallelized whitelist comparison

--- monitor.go
+++ monitor.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"github.com/shirou/gopsutil/process"
 )
 
 // Monitor is struct of monitor command
@@ -76,29 +77,60 @@ func (monitor *Monitor) Monitor() error {
 
 func (monitor *Monitor) psCheck() {
 	fmt.Println("Checking processes infomretions")
-	targets, err := getProcessesInfo(execFlag | cmdFlag | userFlag | pidFlag | openFlag)
+	targets := make([]Target, 0)
+	pses, err := process.Processes()
 	if err != nil {
-		log.Printf("Error: %s\n", errors.Wrap(err, "cause in psCheck"))
-	}
-	for _, target := range targets {
-		sort.Slice(target.Open, func(i, j int) bool { return target.Open[i] < target.Open[j] })
+		log.Printf("Error: %s", errors.Wrap(err, "getProcessesInfo: processes"))
 	}
 	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go monitor.checkWhite(targets, wg)
-	wg.Add(1)
-	go monitor.checkBlack(targets, wg)
-	wg.Wait()
+	ch := make(chan Target, 100)
+	checkTypes := execFlag | cmdFlag | userFlag | pidFlag | openFlag
+	for _, ps := range pses {
+		wg.Add(1)
+		go getProcessInfo(checkTypes, ps, wg, ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+	wgb := &sync.WaitGroup{}
+	for target := range ch {
+		sort.Slice(target.Open, func(i, j int) bool { return target.Open[i] < target.Open[j] })
+		wgb.Add(1)
+		go monitor.checkBlack(target, wgb)
+		targets = append(targets, target)
+	}
+	wgw := &sync.WaitGroup{}
+	wgw.Add(1)
+	go monitor.checkWhiteAll(targets, wgw)
+	wgw.Wait()
+	wgb.Wait()
 }
 
-func (monitor *Monitor) checkWhite(targets []Target, wg *sync.WaitGroup) {
-	defer wg.Done()
+type foundTarget struct {
+	found  bool
+	target Target
+}
+
+func (monitor *Monitor) checkWhiteAll(targets []Target, wgp *sync.WaitGroup) {
+	defer wgp.Done()
 	timeString := time.Now().Format("2006/01/02 15:04:05")
 
+	wg := &sync.WaitGroup{}
+	ch := make(chan foundTarget, 100)
 	for _, white := range monitor.whitelist {
-		found := monitor.isWhite(white, targets)
-		if !found {
-			outputTxt := fmt.Sprintf("%s NotFound: %v\n", timeString, white)
+		wg.Add(1)
+		go monitor.isWhite(white, targets, wg, ch)
+	}
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for result := range ch {
+		if !result.found {
+			outputTxt := fmt.Sprintf("%s NotFound: %v\n", timeString, result.target)
 			if err := appendFile(monitor.outputPath, outputTxt); err != nil {
 				log.Print(errors.Wrap(err, "couse in checkWhite"))
 			}
@@ -106,86 +138,103 @@ func (monitor *Monitor) checkWhite(targets []Target, wg *sync.WaitGroup) {
 	}
 }
 
-func (monitor *Monitor) checkBlack(targets []Target, wg *sync.WaitGroup) {
-	defer wg.Done()
+func (monitor *Monitor) checkBlack(target Target, wgp *sync.WaitGroup) {
+	defer wgp.Done()
 	timeString := time.Now().Format("2006/01/02 15:04:05")
-
-	for _, target := range targets {
-		found := monitor.isBlack(target, monitor.blacklist)
-		if found {
-			outputTxt := fmt.Sprintf("%s WARNING!! FOUND!!: %v\n", timeString, target)
-			if err := appendFile(monitor.outputPath, outputTxt); err != nil {
-				log.Print(errors.Wrap(err, "couse in checkBlack"))
-			}
+	found := monitor.isBlack(target)
+	if found {
+		outputTxt := fmt.Sprintf("%s WARNING!! FOUND!!: %v\n", timeString, target)
+		if err := appendFile(monitor.outputPath, outputTxt); err != nil {
+			log.Print(errors.Wrap(err, "couse in checkBlack"))
 		}
 	}
 }
 
-func (monitor *Monitor) isWhite(white Target, targets []Target) bool {
-	found := false
+func (monitor *Monitor) isWhite(white Target, targets []Target, wgp *sync.WaitGroup, chp chan foundTarget) {
+	defer wgp.Done()
+
+	wg := &sync.WaitGroup{}
+	ch := make(chan bool)
 	for _, target := range targets {
-		found = true
-		if white.Exec != "" && white.Exec != target.Exec {
-			found = false
-		}
-		if white.Cmd != "" && white.Cmd != target.Cmd {
-			found = false
-		}
-		if len(white.Open) != 0 {
-			if fmt.Sprintf("%v", white.Open) != fmt.Sprintf("%v", target.Open) {
-				found = false
-			}
-		}
-		if white.User != "" && white.User != target.User {
-			found = false
-		}
-		if white.Pid != 0 && white.Pid != target.Pid {
-			found = false
-		}
-		if white.Regexp != "" {
-			reFlag := false
-			for x := 0; x < 1; x++ {
-				if ok, err := monitor.matchPattern(target.Exec, white.Regexp); err != nil {
-					log.Print(errors.Wrap(err, "couse in isWhite"))
-				} else if ok {
-					reFlag = true
-					break
-				}
-				if ok, err := monitor.matchPattern(target.Cmd, white.Regexp); err != nil {
-					log.Print(errors.Wrap(err, "couse in isWhite"))
-				} else if ok {
-					reFlag = true
-					break
-				}
-				if ok, err := monitor.matchPattern(target.User, white.Regexp); err != nil {
-					log.Print(errors.Wrap(err, "couse in isWhite"))
-				} else if ok {
-					reFlag = true
-					break
-				}
-				for _, open := range target.Open {
-					if ok, err := monitor.matchPattern(open, white.Regexp); err != nil {
-						log.Print(errors.Wrap(err, "couse in isWhite"))
-					} else if ok {
-						reFlag = true
-						break
-					}
-				}
-			}
-			if !reFlag {
-				found = false
-			}
-		}
+		wg.Add(1)
+		go monitor.isWhiteFunc(white, target, wg, ch)
+	}
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+	var found bool
+	for found = range ch {
 		if found {
 			break
 		}
 	}
-	return found
+
+	result := foundTarget{found, white}
+	chp <- result
 }
 
-func (monitor *Monitor) isBlack(target Target, blacks []Target) bool {
+func (monitor *Monitor) isWhiteFunc(white Target, target Target, wgp *sync.WaitGroup, ch chan bool) {
+	defer wgp.Done()
+
+	found := true
+	if white.Exec != "" && white.Exec != target.Exec {
+		found = false
+	}
+	if white.Cmd != "" && white.Cmd != target.Cmd {
+		found = false
+	}
+	if len(white.Open) != 0 {
+		if fmt.Sprintf("%v", white.Open) != fmt.Sprintf("%v", target.Open) {
+			found = false
+		}
+	}
+	if white.User != "" && white.User != target.User {
+		found = false
+	}
+	if white.Pid != 0 && white.Pid != target.Pid {
+		found = false
+	}
+	if white.Regexp != "" {
+		reFlag := false
+		for x := 0; x < 1; x++ {
+			if ok, err := monitor.matchPattern(target.Exec, white.Regexp); err != nil {
+				log.Print(errors.Wrap(err, "couse in isWhite"))
+			} else if ok {
+				reFlag = true
+				break
+			}
+			if ok, err := monitor.matchPattern(target.Cmd, white.Regexp); err != nil {
+				log.Print(errors.Wrap(err, "couse in isWhite"))
+			} else if ok {
+				reFlag = true
+				break
+			}
+			if ok, err := monitor.matchPattern(target.User, white.Regexp); err != nil {
+				log.Print(errors.Wrap(err, "couse in isWhite"))
+			} else if ok {
+				reFlag = true
+				break
+			}
+			for _, open := range target.Open {
+				if ok, err := monitor.matchPattern(open, white.Regexp); err != nil {
+					log.Print(errors.Wrap(err, "couse in isWhite"))
+				} else if ok {
+					reFlag = true
+					break
+				}
+			}
+		}
+		if !reFlag {
+			found = false
+		}
+	}
+	ch <- found
+
+}
+func (monitor *Monitor) isBlack(target Target) bool {
 	found := false
-	for _, black := range blacks {
+	for _, black := range monitor.blacklist {
 		found = true
 		if black.Exec != "" && target.Exec != black.Exec {
 			found = false
